1f938400adaa24b804dd4b3ee618e4e79f1e9369
pooja-mittal068 <pooja.mittal068@gmail.com>
2024-08-20 15:08:00 +0530

    data ingestion completed

--- template.py
+++ template.py
@@ -20,13 +20,15 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/utils/common.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py"
+    "templates/index.html",
+    "main.py"
 ]
 
 for filepath in list_of_files:
