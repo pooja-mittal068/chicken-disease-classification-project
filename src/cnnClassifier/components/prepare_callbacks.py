c57b2babe599913e721b9ceb9a8d5c5c5d7b40c0
pooja-mittal068 <pooja.mittal068@gmail.com>
2024-08-21 16:47:20 +0530

    training added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -26,8 +26,7 @@ class PrepareCallback:
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
             filepath=self.config.checkpoint_model_filepath,
-            save_best_only=True,
-            save_weights_only=True
+            save_best_only=True
         )
 
 
